74592e95bdcbccfd8e005d65666e7d93b2b732d9
melisacakici <81982736+melisacakici@users.noreply.github.com>
2021-04-07 01:48:03 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,9 @@
-#Explain your work
+# Python homework 1
 
-#Question 1
-for x in range(a):
-	print(a)
+#Question 1 : creating a list and swap half of the list with other half
+
+list = [1,2,3,4,5,6,7,8]
+l = len(list)
+m=l//2
+list[:m],list[m:] = list[m:],list[:m] 
+print(list)
